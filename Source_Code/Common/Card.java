94af3d71adc468752800722e683d23e6610d35d0
dav96che <dcheng29@jh.edu>
2021-03-13 21:13:27 -0500

    Modified card classes to allow for more customizability.
    Cards now have a flag that can be set to indicate that they have custom
    names. Need to write method that reads/loads the custom names from a
    property/xml file.

--- Source_Code/Common/Card.java
+++ Source_Code/Common/Card.java
@@ -9,9 +9,43 @@ package Common;
 public class Card
 {
 
+   private boolean nameModified = false;
+   
+   
    Card()
    {
 
    }
 
+   
+   
+   /**
+    * Sets an indicator so that the program knows that the card name needs
+    * to be overwritten. This lets us keep the default names hard-coded in the
+    * program as backups.
+    */
+   public void modifyName()
+   {
+      this.nameModified = true;
+
+   }
+
+
+   /**
+    * Sets the modified name toggle to false. This will cause the program to
+    * use default names rather than character defined names.
+    */
+   public void resetName()
+   {
+      this.nameModified = false;
+
+   }
+   
+   
+   public boolean isNameModified()
+   {
+      return this.nameModified;
+   }
+   
+   
 }
--- Source_Code/Common/CharacterCard.java
+++ Source_Code/Common/CharacterCard.java
@@ -3,7 +3,22 @@ package Common;
 public class CharacterCard extends Card
 {
 
-   // Must instantiate the character card with the specific character
+   enum CharacterName
+   {
+      COLONEL_MUSTARD, MISS_SCARLET, PROFESSOR_PLUM, MR_GREEN, MRS_WHITE,
+      MRS_PEACOCK;
+   }
+
+
+   CharacterName characterName;
+
+
+   /**
+    * Must instantiate the character card with the specific character
+    * 
+    * @param characterName
+    *        Name of the character
+    */
    CharacterCard( CharacterName characterName )
    {
 
@@ -21,25 +36,59 @@ public class CharacterCard extends Card
    }
 
 
+   /**
+    * Instantiate the character card with the specific character name and pass
+    * a boolean representing whether or not a custom name should be used
+    * 
+    * @param characterName
+    *        The name of the Character
+    * @param modifyCharacterName
+    *        True if custom name is used
+    */
+   CharacterCard( CharacterName characterName, boolean modifyCharacterName )
+   {
+
+      try
+      {
+         this.characterName = characterName;
+
+         if( modifyCharacterName )
+         {
+            this.modifyName();
+         }
+
+      }
+
+      // Guards for invalid character names
+      catch( Exception e )
+      {
+         e.printStackTrace();
+      }
+
+   }
+
+   
+   /**
+    * Looks into a custom file, reads the name associated with the assigned
+    * character name, and replaces the character name with the custom name
+    */
+   public void loadCustomCharacterName()
+   {
+      
+   }
+   
+
+   /**
+    * @return The specific character name represented by this instance
+    */
    public String getCharacterName()
    {
       return this.characterName.toString();
 
    }
 
-
-   enum CharacterName
-   {
-      COLONEL_MUSTARD, MISS_SCARLET, PROFESSOR_PLUM, MR_GREEN, MRS_WHITE,
-      MRS_PEACOCK;
-   }
-
-
-   CharacterName characterName;
-
    /*
-    * Test Method 
-    * public static void main( String[] args ) {
+    * Test Method public static void main( String[] args ) {
     * 
     * CharacterName name = CharacterName.MR_GREEN; CharacterCard test = new
     * CharacterCard( name );
