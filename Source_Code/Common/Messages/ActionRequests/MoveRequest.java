c4ae623f3709858bd2e010a962ee2d91baeefa40
Nicholas Blair <NBlair52@gmail.com>
2021-03-21 19:28:31 -0400

    Updated MoveRequest to include PlayerID
    Uses super(PlayerID) in order to invoke ActionRequest(PlayerID) since all Action Requests will include a PlayerID

--- Source_Code/Common/Messages/ActionRequests/MoveRequest.java
+++ Source_Code/Common/Messages/ActionRequests/MoveRequest.java
@@ -3,19 +3,19 @@ package Common.Messages.ActionRequests;
 
 public class MoveRequest extends ActionRequest
 {
-   
+
    /**
     * Using a string to keep track of where the player wants to move. This seemed the
-    * most flexible until we figure out if we want to use room names to dictate 
+    * most flexible until we figure out if we want to use room names to dictate
     * movement, or if we want to use directions.
     */
    public String moveDirection;
-   
+
    public int PlayerID;
-   
+
    public MoveRequest( int PlayerID, String moveDirection )
    {
-      super();
+      super(PlayerID);
       this.PlayerID = PlayerID;
       this.moveDirection = moveDirection;
    }
