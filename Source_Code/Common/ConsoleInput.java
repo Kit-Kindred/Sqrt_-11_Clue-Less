65e56b2b24ed093ccffac6584d286ce527436e44
dav96che <dcheng29@jh.edu>
2021-04-11 19:25:00 -0400

    Input stream refactoring and Added Suggestion Logic
    Removed the scanner class in favor of a more custom, thread-safe
    operation (BufferedInputStream). Added message handling for the newly
    created suggestion logic to allow players to choose the card they want
    to refute the suggestion with.

---
+++ Source_Code/Common/ConsoleInput.java
@@ -0,0 +1,97 @@
+package Common;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.concurrent.*;
+
+
+/**
+ * Thread safe input object we can use instead of scanner.
+ * 
+ * https://www.javaspecialists.eu/archive/Issue153-Timeout-on-Console-Input.html
+ *
+ */
+public class ConsoleInput
+{
+
+   public String read() throws IOException
+   {
+      ExecutorService ex    = Executors.newSingleThreadExecutor();
+      String          input = null;
+
+      try
+      {
+
+            Future< String > result = ex.submit( new ConsoleInputHandler() );
+
+            try
+            {
+               input = result.get();
+            } 
+            catch( ExecutionException e )
+            {
+               e.getCause().printStackTrace();
+            } 
+            catch( InterruptedException e )
+            {
+               e.printStackTrace();
+            }
+
+      } finally
+      {
+         ex.shutdownNow();
+      }
+
+      return input;
+
+   }
+
+   
+   /**
+    * Need a better way of handling inputs. Using scanner causing blocking in the
+    * main thread which doesn't allow for easy context switches.
+    * https://stackoverflow.com/questions/4983065/how-to-interrupt-java-util-scanner-nextline-call
+    *
+    */
+   public class ConsoleInputHandler implements Callable< String >
+   {
+
+      public String call() throws IOException
+      {
+         BufferedReader br = new BufferedReader(
+            new InputStreamReader( System.in ) );
+         String input;
+
+         do
+         {
+            System.out.print( ">> " );
+
+            input = br.readLine();
+            
+            
+//            try
+//            {
+//
+//               // wait until we have data to complete a readLine()
+//               while( !br.ready() /* ADD SHUTDOWN CHECK HERE */ )
+//               {
+//                  Thread.sleep( 200 );
+//               }
+//
+//               input = br.readLine();
+//            } catch( InterruptedException e )
+//            {
+//               return null;
+//            }
+
+         } while( "".equals( input ) );
+
+         return input;
+
+      }
+
+   }
+}
+
